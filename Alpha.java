d3fa5962b92af78ce02b2bc94673aa8239b222e6
Siddhant Fatewar <sidfatewar30@gmail.com>
2023-04-24 04:43:39 +0530

    Added Beta file and modified Alpha file

--- Alpha.java
+++ Alpha.java
@@ -2,6 +2,9 @@ class Alpha
 {
     public static void main(String[] args) {
         System.out.println("Sid");
-        
+        System.out.println("Sid");
+        System.out.println("Sid");
+        System.out.println("Sid");
+        System.out.println("Sid");
     }
 }
